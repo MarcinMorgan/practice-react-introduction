f75d72b1e477053911e4d5a9e531209e515210b8
MarcinMorgan <morgangzit@gmail.com>
2024-09-04 15:22:50 +0200

    t5 removed unnecessary variable

--- 05/Menu.js
+++ 05/Menu.js
@@ -3,9 +3,8 @@ import MenuItem from "./MenuItem";
 
 class Menu extends React.Component {
     render() {
-        const elements = this.props.items.map(elem => {return <MenuItem {...elem}/>})
         return <ul>
-            {elements}
+            {this.props.items.map(elem => {return <MenuItem {...elem}/>})}
         </ul>
         }
 }
